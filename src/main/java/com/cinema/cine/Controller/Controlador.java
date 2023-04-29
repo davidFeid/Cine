ae4b07ad031db0c29fdb5f4fae53b47d614a0b0c
civic100 <114212306+civic100@users.noreply.github.com>
2023-04-29 16:43:02 +0200

    butacas/CRUDRepo
    En la sección de crudrepor en la parte del controlador se han añadido todos los srevis en el, (comentar posivliada de separar el controller.)

--- src/main/java/com/cinema/cine/Controller/Controlador.java
+++ src/main/java/com/cinema/cine/Controller/Controlador.java
@@ -1,8 +1,10 @@
 package com.cinema.cine.Controller;
 
+import com.cinema.cine.Entity.Butaca;
 import com.cinema.cine.Entity.Pelicula;
 import com.cinema.cine.Entity.Sala;
 import com.cinema.cine.Entity.Usuario;
+import com.cinema.cine.Service.ButacaServiceIMPL.BSIMPL;
 import com.cinema.cine.Service.SalaServiceIMPL.SSIMPL;
 import com.cinema.cine.Service.PersonaServiceIMPL.PSIMPL;
 import com.cinema.cine.Service.UusuarioServiceIMPL.USIMPL;
@@ -17,13 +19,16 @@ import java.util.List;
 @RestController
 @CrossOrigin("*")
 public class Controlador {
-
-    private final PSIMPL psimpl;
-
-    public Controlador(PSIMPL psimpl) {
+    public Controlador(PSIMPL psimpl, SSIMPL ssimpl, USIMPL usimpl, BSIMPL bsimpl) {
         this.psimpl = psimpl;
+        this.ssimpl = ssimpl;
+        this.usimpl = usimpl;
+        this.bsimpl = bsimpl;
     }
-
+    private final PSIMPL psimpl;
+    private final SSIMPL ssimpl;
+    private  final  BSIMPL bsimpl;
+    private final USIMPL usimpl;
 
     //Peliculas
     @GetMapping
@@ -61,8 +66,6 @@ public class Controlador {
         return ResponseEntity.ok().build();
     }
 
-    @Autowired
-    private SSIMPL ssimpl;
 
     //Salas
     @GetMapping
@@ -100,10 +103,44 @@ public class Controlador {
         return ResponseEntity.ok().build();
     }
 
-    //Usuarios
-    @Autowired
-    private USIMPL usimpl;
+    //Butacas
+    @GetMapping
+    @RequestMapping(value = "ConsultarButaca",method =RequestMethod.GET)
+    public ResponseEntity<?> ConsultarButaca(){
+        List<Butaca> listarButaca=this.bsimpl.ConsultarButaca();
+        return ResponseEntity.ok(listarButaca);
+    }
 
+    @PostMapping
+    @RequestMapping(value = "CrearButaca",method =RequestMethod.POST)
+    public ResponseEntity<?> CrearButaca(@RequestBody Butaca butaca){
+        Butaca ButacaCreada=this.bsimpl.CrearButaca(butaca);
+        return ResponseEntity.status(HttpStatus.CREATED).body(ButacaCreada);
+    }
+
+    @PutMapping
+    @RequestMapping(value = "ModificarButaca",method =RequestMethod.PUT)
+    public ResponseEntity<?> ModificarButaca(@RequestBody Butaca butaca){
+        Butaca ButacaModificada=this.bsimpl.ModificarButaca(butaca);
+        return ResponseEntity.status(HttpStatus.CREATED).body(ButacaModificada);
+    }
+
+    @GetMapping
+    @RequestMapping(value = "BuscarButaca/{id}",method =RequestMethod.GET)
+    public ResponseEntity<?> BuscarButaca(@PathVariable int id){
+        Butaca butaca=this.bsimpl.BuscarButaca(id);
+        return ResponseEntity.ok(butaca);
+    }
+
+    @DeleteMapping
+    @RequestMapping(value = "EliminarButaca/{id}",method =RequestMethod.DELETE)
+    public ResponseEntity<?> ElimnarButaca(@PathVariable int id){
+        this.bsimpl.EliminarButaca(id);
+        return ResponseEntity.ok().build();
+    }
+
+
+    //Usuarios
     @GetMapping
     @RequestMapping(value = "ConsultarUsuario",method =RequestMethod.GET)
     public ResponseEntity<?> ConsultarUsuario(){
@@ -128,7 +165,6 @@ public class Controlador {
         Usuario usuario=this.usimpl.BuscarUsuario(id);
         return ResponseEntity.ok(usuario);
     }
-
     @DeleteMapping
     @RequestMapping(value = "EliminarUsuario/{id}",method =RequestMethod.DELETE)
     public ResponseEntity<?> EliminarUsuario(@PathVariable int id){
